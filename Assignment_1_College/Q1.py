8b240d5bb62a1bd39bcfdca88430c445908a5196
harshGitprofile <hraj09590@gmail.com>
2026-09-22 18:46:03 +0530

    This is AS-1 Q1.

---
+++ Assignment_1_College/Q1.py
@@ -0,0 +1,9 @@
+#1.	Write a Python program to print your name, branch, roll number, and college name on separate lines. 
+name = input("Enter Your name: ")
+branch = input("Enter Your Branch: ")
+Roll = int(input("Enter Your roll no. : "))
+college = input("Enter Your  college name: ")
+
+print("----Student Profile----")
+print(f"Name : {name}\nBranch : {branch}\nRoll no. : {Roll}\nInstitute name : {college}")
+print("----------XXX----------")

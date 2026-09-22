ac2900d8428d6df4b60fa8c349a311f33b4548b0
harshGitprofile <hraj09590@gmail.com>
2026-09-22 18:53:51 +0530

    this is Q2.

---
+++ Assignment_1_College/Q2.py
@@ -0,0 +1,15 @@
+"""
+2.	Write a program to input two numbers and display: 
+•	Sum 
+•	Difference 
+•	Product 
+•	division 
+•	Floor division 
+•	Remainder 
+"""
+a = int(input("Enter the value of a : "))
+b = int(input("Enter the value of b : "))
+print(f"Sum of {a} and {b} is {a+b}\nDifference of {a} and {b} is {a-b}\nProduct of {a} and {b} is {a*b}")
+print(f"Division of {a} and {b} is {a/b}\nFloor Division of {a} and {b} is {a//b}\n")
+print(f"Remainder of {a} and {b} is {a%b}")
+

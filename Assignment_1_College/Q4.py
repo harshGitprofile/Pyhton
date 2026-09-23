98c679643c80d9eac29450607e3b9f621fb10e4b
harshGitprofile <hraj09590@gmail.com>
2026-09-23 16:49:31 +0530

    This is the newest updated file.

--- Assignment_1_College/Q4.py
+++ Assignment_1_College/Q4.py
@@ -6,3 +6,6 @@ rad = int(input("Enter the radius of circle in cm:"))
 area,peri  = 3.14 * (rad ** 2),(2*3.14*rad)
 print(f"Area of circle of radius {rad}cm is {area}cm^2")
 print(f"Circumference of circle of radius {rad}cm is  {peri}cm")
+
+print("mai change hua hu re dekh mai kha kha badhla hu dm hai to dekh.")
+# now changed

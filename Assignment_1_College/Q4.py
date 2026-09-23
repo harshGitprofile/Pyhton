baad1e6ce12a2cf8bd634db693bdd3b998ba6098
harshGitprofile <hraj09590@gmail.com>
2026-09-23 16:06:07 +0530

    this is file 4 of python.

---
+++ Assignment_1_College/Q4.py
@@ -0,0 +1,8 @@
+"""
+4.	Write a program to input radius from user and calculate the
+area and circumference of a circle. 
+"""
+rad = int(input("Enter the radius of circle in cm:"))
+area,peri  = 3.14 * (rad ** 2),(2*3.14*rad)
+print(f"Area of circle of radius {rad}cm is {area}cm^2")
+print(f"Circumference of circle of radius {rad}cm is  {peri}cm")
